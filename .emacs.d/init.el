9ce525e8cbd3fdee8d6b6e23314933b9396e66e2
Trent Ogren <tedwardo2@gmail.com>
2012-08-24 08:44:09 -0500

    Emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,36 @@
+(when (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(starter-kit
+                      starter-kit-bindings
+                      starter-kit-lisp
+                      starter-kit-ruby
+                      starter-kit-js
+                      evil
+                      haml-mode
+                      magit
+                      nrepl
+                      yaml-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(setq-default show-trailing-whitespace t)
+
+(require 'evil)
+;; (evil-mode 1)
+;; (add-hook 'find-file-hooks (lambda () (evil-local-mode nil)))
+
+; YAML
+(autoload 'yaml-mode "yaml-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
